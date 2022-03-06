4632d27aedd55e4dc64daa0e2c00318300076cbf
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-07 00:36:25 +0800

    Update day.js

--- day.js
+++ day.js
@@ -1,1 +1,21 @@
-// to do
+module.exports = {
+  name: "day",
+  run: async (message, args, client) => {
+    
+    let wolves = require("./day/wolves.js")
+    let serialkillers = require("./day/wolves.js")
+    let bandits = require("./day/wolves.js")
+    let cannibals = require("./day/wolves.js")
+    let zombies = require("./day/wolves.js")
+    let corruptors = require("./day/wolves.js")
+    let arsonists = require("./day/wolves.js")
+    let bombers = require("./day/wolves.js")
+    let illusionists = require("./day/wolves.js")
+    let alchemists = require("./day/wolves.js")
+    let sectleader = require("./day/wolves.js")
+    let grumpygrandmas = require("./day/wolves.js")
+    let beasthunters = require("./day/wolves.js")
+    
+  }
+}
+
