0234deb6ececbbc33810e4a5cc1786c236924ee6
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-17 09:45:45 +0800

    Update serialkiller.js

--- day/serialkiller.js
+++ day/serialkiller.js
@@ -1,1 +1,16 @@
-// to do
+const db = require("quick.db") // database
+const { getRole, getEmoji } = require("../../../config") // functions
+const doctor = require("./protection/doctor.js") // doctor protection
+const beastHunter = require("./protection/beastHunter.js") // beast hunter protection
+const witch = require("./protection/witch.js") // witch protection
+const jailer = require("./protection/jailer.js") // jailer protection
+const redLady = require("./protection/redLady.js") // red lady protection
+const bodyguard = require("./protection/bodyguard.js") // bodyguard protection
+const toughGuy = require("./protection/toughGuy.js") // tough guy protection
+const forger = require("./protection/forger.js") // forger protection
+const ghostLady = require("./protection/ghostLady.js") // ghost lady protection
+
+module.exports = client => {
+
+  
+}
