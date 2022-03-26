43b2914d9cbe7a22aa9719514aa60c52a94e3ab0
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-26 23:22:56 +0800

    Update serialkillers.js

--- day/serialkillers.js
+++ day/serialkillers.js
@@ -92,7 +92,13 @@ module.exports = async (client, alivePlayersBefore) => {
           db.set(`player_${result.id}.status`, "Dead")          
           let attackedPlayer = await guild.members.fetch(result.id) // fetch the discord member - Object
           let attackedPlayerRoles = attackedPlayer.roles.cache.map(r => r.name === "Alive" ? "892046207428476989" : r.id) // get all the roles and replace the Alive role with Dead.
-          await dayChat.send(`${getEmoji("serial_killer_knife", client)} The Serial Killer stabbed **${players.indexOf(result.id)+1} ${result.username} (${getEmoji(result.role?.toLowerCase()?.replace(/\s/g, "_"), client)} ${result.role})**!`)
+          
+          // check if they were hypnotized
+          if (typeof attacker.hypnotize === "string") {
+            await dayChat.send(`${getEmoji("serial_killer_knife", client)} The Dreamcatcher compelled the Serial Killer to stab **${players.indexOf(result.id)+1} ${result.username} (${getEmoji(result.role?.toLowerCase()?.replace(/\s/g, "_"), client)} ${result.role})**!`)
+          } else {
+            await dayChat.send(`${getEmoji("serial_killer_knife", client)} The Serial Killer stabbed **${players.indexOf(result.id)+1} ${result.username} (${getEmoji(result.role?.toLowerCase()?.replace(/\s/g, "_"), client)} ${result.role})**!`)
+          }
           await attackedPlayer.roles.set(attackedPlayerRoles) // removes the Alive and adds the Dead discord role
           
         } else { // otherwise they were protected
